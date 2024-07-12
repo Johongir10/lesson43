feed5ec1613d8b3b9556adf121b3c4065c9def5a
Johongir10 <shavkatovj73@gmail.com>
2024-07-12 10:34:55 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,20 @@
+let students=[ "Amirxon ustoz","Jahongir","Abduazm","Muhammadamin","Jovohir"]
+
+
+
+
+
+
+console.log(students);
+alert("Arrayingizdan uzunligi:" + students.length );
+let javob=confirm("Arraydan oxiridan bittasini ochirmoqchimisiz");
+
+
+if(javob==true){
+    satudents.pop();
+    alert("Arraying uzunligini: " + students.length);
+    console.log(students);
+}else{
+    alert("Arraying uzunligi: " +students.length);
+    console.log(students)
+}
